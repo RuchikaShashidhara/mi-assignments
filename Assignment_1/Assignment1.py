f30d0b9c762557e923b6fab687643906e2e2db20
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2020-09-02 10:59:44 +0530

    removed eps & added continue instead

--- Assignment_1/Assignment1.py
+++ Assignment_1/Assignment1.py
@@ -1,9 +1,7 @@
-'''
-Assume df is a pandas dataframe object of the dataset given
-'''
 import numpy as np
 import pandas as pd
 import random
+
 
 '''Calculate the entropy of the enitre dataset'''
 def get_entropy_of_dataset(df):
@@ -17,12 +15,13 @@ def get_entropy_of_dataset(df):
 
     # Sumation of -pi*log(pi)
     for tar_val in target_values:
-        pi = df[lastColName].value_counts()[tar_val] / len(df[lastColName])  # epsilon not needed
-        entropy += -(pi * np.log2(pi + np.finfo(float).eps))
+        pi = df[lastColName].value_counts()[tar_val] / len(df[lastColName])
+        if(pi == 0):
+            continue
+        entropy += -(pi * np.log2(pi))
 
     # Return Entropy(S) of dataset
     return entropy
-
 
 
 '''Return entropy of the attribute provided as parameter'''
@@ -50,19 +49,20 @@ def get_entropy_of_attribute(df,attribute):
                 # Find pi numerator
                 pi_num = len(df[attribute][df[attribute] == att_val][df[lastColName] == tar_val])
                 # Find pi
-                pi = pi_num / pi_den # epsilon not needed
+                pi = pi_num / pi_den
                 # Accumulate Feature Entropy(A=att_val)
-                entropy_of_feature += -(pi * np.log2(pi + np.finfo(float).eps))
+                if(pi == 0):
+                    continue
+                entropy_of_feature += -(pi * np.log2(pi))
 
             # Accumulate Attribute Entropy(A=att_val)
-            entropy_of_attribute += ((pi_den/len(df) * entropy_of_feature))
+            entropy_of_attribute += (((pi_den/len(df)) * entropy_of_feature))
 
     except KeyError:
         print("Attribute:", attribute, "not in dataset")
 
     # Return Attribute Entropy(A)
     return entropy_of_attribute
-
 
 
 '''Return Information Gain of the attribute provided as parameter'''
@@ -80,8 +80,7 @@ def get_information_gain(df,attribute):
     return information_gain
 
 
-
-''' Returns Attribute with highest info gain'''
+''' Return Attribute with highest info gain'''
 def get_selected_attribute(df):
 
     # Obtain list of attributes, assuming the last column is the decision taken.
@@ -101,18 +100,7 @@ def get_selected_attribute(df):
     Return a tuple with the first element as a dictionary which has IG of all columns
     and the second element as a string with the name of the column selected
 
-    example : ({'A':0.123,'B':0.768,'C':1.23} , 'C')
-    '''
+    example : ({'A':0.123,'B':0.768,'C':1.23} , 'C')    '''
 
     return (information_gains,selected_column)
 
-
-
-'''
-------- TEST CASES --------
-How to run sample test cases ?
-
-Simply run the file DT_SampleTestCase.py
-Follow convention and do not change any file / function names
-
-'''
