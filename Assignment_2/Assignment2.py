04ecc21b66342b0049a1655c016c3ef677e509db
Kavya-Shekar <kavyay2k@gmail.com>
2020-09-20 12:05:04 +0530

    Added code for A_star search

--- Assignment_2/Assignment2.py
+++ Assignment_2/Assignment2.py
@@ -3,10 +3,45 @@ import copy
 
 def A_star_Traversal(cost, heuristic, start_point, goals):
 
-    # Parameters
-    A_star_path = []
+    n = len(cost)                                               # Number of Nodes in Graph
+    visited = [0 for i in range(n)]                             # Visited Set (0 - not visited, 1 - visited)
+    frontier_priority_queue = queue.PriorityQueue(maxsize = n)  # Frontier Priority Queue
 
-    return A_star_path
+    # Insert (estimated_total_cost, (start_point, A*_path_till_start_node, 0)) into priority queue
+    frontier_priority_queue.put((heuristic[start_point], (start_point, [start_point], 0)))
+
+    # Until priority queue is not empty
+    while(frontier_priority_queue.qsize() != 0):
+
+        # Pop the node information from the priority queue
+        total_estimated_cost, nodes_tuple = frontier_priority_queue.get()
+        node = nodes_tuple[0]
+        A_star_path_till_node = nodes_tuple[1]
+        node_cost = nodes_tuple[2]
+
+        # If the node was not visited
+        if visited[node] == 0:
+            visited[node] = 1
+
+            # If node is a goal_point, return the A* Path till Goal Node
+            if node in goals:
+                return A_star_path_till_node
+
+            # For every neighbour connected node
+            for neighbour_node in range(1, n):
+                # If the node is connected and not visited, add it to the priority queue
+                if cost[node][neighbour_node] > 0 and visited[neighbour_node] == 0:
+
+                    # Compute total cost till neighbour node
+                    total_cost_till_node = node_cost + cost[node][neighbour_node]
+                    # Compute estimated total cost for the path
+                    estimated_total_cost = total_cost_till_node + heuristic[neighbour_node]
+
+                    # Add the neighbour node to the new A* path list
+                    A_star_path_till_neighbour_node = copy.deepcopy(A_star_path_till_node)
+                    A_star_path_till_neighbour_node.append(neighbour_node)
+                    # Insert (estimated total, (neighbour_node, A_star_path_till_neighbour_node, total cost till neighbour nodes)) into priority queue
+                    frontier_priority_queue.put((estimated_total_cost, (neighbour_node, A_star_path_till_neighbour_node, total_cost_till_node)))
 
 
 def UCS_Traversal(cost, start_point, goals):
