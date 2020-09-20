8b51bd6cffc02b45a2b7164b8a26eaee6099efec
Ruchika Shashidhara <48530409+RuchikaShashidhara@users.noreply.github.com>
2020-09-20 23:52:25 +0530

    added new stack tuple for dfs paths

--- Assignment_2/Assignment2.py
+++ Assignment_2/Assignment2.py
@@ -3,6 +3,7 @@ import copy
 
 def A_star_Traversal(cost, heuristic, start_point, goals):
 
+    # Parameters
     n = len(cost)                                               # Number of Nodes in Graph
     visited = [0 for i in range(n)]                             # Visited Set (0 - not visited, 1 - visited)
     frontier_priority_queue = queue.PriorityQueue(maxsize = n)  # Frontier Priority Queue
@@ -42,20 +43,16 @@ def A_star_Traversal(cost, heuristic, start_point, goals):
                     A_star_path_till_neighbour_node.append(neighbour_node)
                     # Insert (estimated total, (neighbour_node, A_star_path_till_neighbour_node, total cost till neighbour nodes)) into priority queue
                     frontier_priority_queue.put((estimated_total_cost, (neighbour_node, A_star_path_till_neighbour_node, total_cost_till_node)))
-    
+
     return list()
 
 
 def UCS_Traversal(cost, start_point, goals):
 
     # Parameters
-    #UCS_path = []                                               # UCS Traversal Path
-    # I have just appeneded a list of node paths corresponding to path cost till that node into the PQ itself
-    # its better if we make a backtracking path tree DS ourselves
     n = len(cost)                                               # Number of Nodes in Graph
     visited = [0 for i in range(n)]                             # Visited Set (0 - not visited, 1 - visited)
     frontier_priority_queue = queue.PriorityQueue(maxsize = n)  # Frontier Priority Queue
-    path_found = 0                                              # Flag to check whether a path was found(1)or not(0)
 
     # Insert (0, (start_point, UCS_path_till_start_point)) into priority queue
     frontier_priority_queue.put((0, (start_point, [start_point])))
@@ -67,8 +64,6 @@ def UCS_Traversal(cost, start_point, goals):
         node_cost, nodes_tuple = frontier_priority_queue.get()
         node = nodes_tuple[0]
         ucs_path_till_node = nodes_tuple[1]
-        #print(ucs_path_till_node)
-        #print(node_cost, nodes_tuple)
 
         # If the node was not visited
         if visited[node] == 0:
@@ -76,7 +71,6 @@ def UCS_Traversal(cost, start_point, goals):
 
             # If node is a goal_point, return the UCS Path till Goal Node
             if node in goals:
-                path_found = 1
                 return ucs_path_till_node
 
             # For every neighbour connected node
@@ -93,37 +87,32 @@ def UCS_Traversal(cost, start_point, goals):
                         # Insert (total cost till node, (neighbour_node, UCS_path_till_neighbour_node)) into priority queue
                         frontier_priority_queue.put((total_cost_till_node, (neighbour_node, ucs_path_till_neighbour_node)))
 
-    if(path_found == 0):
-        return list()
+    return list()
 
 
 def DFS_Traversal(cost, start_point, goals):
 
     # Parameters
-    DFS_path = []                                  # DFS Traversal Path
     n = len(cost)                                  # Number of Nodes in Graph
     visited = [0 for i in range(n)]                # Visited Set (0 - not visited, 1 - visited)
     frontier_stack = queue.LifoQueue(maxsize = n)  # Frontier Stack
-    path_found = 0                                 # Flag to check whether a path was found(1)or not(0)
 
     # Insert start_point into stack
-    frontier_stack.put(start_point)
+    frontier_stack.put((start_point, [start_point]))
 
     # Until stack is not empty
     while(frontier_stack.qsize() != 0):
 
         # Pop the node from the stack and append it to the DFS Traversal Path
-        node = frontier_stack.get()
-        DFS_path.append(node)
+        node, dfs_path_till_node = frontier_stack.get()
 
         # If the node was not visited
         if visited[node] == 0:
             visited[node] = 1
 
-            # If node is a goal_point, return the DFS Traversal Path
+            # If node is a goal_point, return the DFS Path till Goal Node
             if node in goals:
-                path_found = 1
-                return DFS_path
+                return dfs_path_till_node
 
             # For every neighbour connected node
             # (taken from n-1 to 1, to get first lexicological path)
@@ -132,39 +121,14 @@ def DFS_Traversal(cost, start_point, goals):
                 if cost[node][neighbour_node] > 0:
                     # If the connected node is not visited, insert it into the stack
                     if visited[neighbour_node] == 0:
-                        frontier_stack.put(neighbour_node)
+                        # Add the neighbour node to the new dfs path list
+                        dfs_path_till_neighbour_node = copy.deepcopy(dfs_path_till_node)
+                        dfs_path_till_neighbour_node.append(neighbour_node)
+                        # Insert ((neighbour_node, DFS_path_till_neighbour_node)) into the stack
+                        frontier_stack.put((neighbour_node, dfs_path_till_neighbour_node))
 
-    if(path_found == 0):
-        return list()
+    return list()
 
-
-# Let's remove this definition comment later on
-'''
-Function tri_traversal - performs DFS, UCS and A* traversals and
-returns the path for each of these traversals
-
-n - Number of nodes in the graph
-m - Number of goals ( Can be more than 1)
-1<=m<=n
-Cost - Cost matrix for the graph of size (n+1)x(n+1)
-IMP : The 0th row and 0th column is not considered as the
-starting index is from 1 and not 0.
-Refer the sample test case to understand this better
-
-Heuristic - Heuristic list for the graph of size 'n+1'
-IMP : Ignore 0th index as nodes start from index value of 1
-Refer the sample test case to understand this better
-
-start_point - single start node
-goals - list of size 'm' containing 'm' goals to reach from start_point
-
-Return : A list containing a list of all traversals [[],[],[]]
-1<=m<=n
-cost[n][n] , heuristic[n], start_point, goals[m]
-
-NOTE : you are allowed to write other helper functions that you
-can call in the given fucntion
-'''
 
 def tri_traversal(cost, heuristic, start_point, goals):
 
